2d7f18db14490727734379a9fd5af6fbd9adec0c
mainm0e <mainm0e1996@gmail.com>
2023-05-04 18:33:27 +0300

    very nice job today

--- optimizer/optimizer.go
+++ optimizer/optimizer.go
@@ -1,11 +1,26 @@
 package optimizer
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"strings"
 )
+
+func byteToString(data [][][]byte) [][][]string {
+	result := make([][][]string, len(data))
+	for i := range data {
+		result[i] = make([][]string, len(data[i]))
+		for j := range data[i] {
+			result[i][j] = make([]string, len(data[i][j]))
+			for k := range data[i][j] {
+				result[i][j][k] = string(data[i][j][k])
+			}
+		}
+	}
+	return result
+}
 
 // Tetris is the main function of the program. It takes a file path as an argument and returns a string.
 func Tetris(example string) (string, error) {
@@ -20,8 +35,35 @@ func Tetris(example string) (string, error) {
 	if coffe == false {
 		return "Invalid file format", nil
 	}
-	moreCoffe, err := cutShape(data)
-	fmt.Println(moreCoffe)
+	obj := bytes.Split(data, []byte{'\n', '\n'})
+	lines := [][][]byte{}
+	for _, v := range obj {
+		lines = append(lines, bytes.Split(v, []byte{'\n'}))
+	}
+
+	expect := [][][]byte{}
+	for i := 0; i < len(lines); i++ {
+		value1, err := cutShape(lines[i])
+		value := rollShape(value1)
+		actual, _ := cutShape(value)
+		value = rollShape(actual)
+		if err != nil {
+			fmt.Println("Failed to cut shape:", err)
+		}
+		expect = append(expect, value)
+	}
+	stringMap := byteToString(expect)
+	tetrisShape := []string{}
+	for i := 0; i < len(stringMap); i++ {
+		vShape := readShapeMap(stringMap[i])
+		tetrisShape = append(tetrisShape, vShape)
+	}
+	for i := 0; i < len(tetrisShape); i++ {
+		if tetrisShape[i] == "Unknown" {
+			return "Unknown shape", errors.New("Unknown shape")
+		}
+	}
+	/* 	fmt.Println(tetrisShape) */
 	return "Tetris", nil
 }
 
@@ -29,11 +71,36 @@ func Tetris(example string) (string, error) {
 // cutShape takes a slice of bytes as an argument and returns a slice of bytes and an error.
 // byte # = 35 and byte . = 46 and byte \n = 10
 
-func cutShape(data []byte) ([]byte, error) {
-	for i := 0; i < len(data); i++ {
-
+func cutShape(lines [][]byte) ([][]byte, error) {
+	var shape [][]byte
+	for i := 0; i < len(lines); i++ {
+		for j := 0; j < len(lines[i]); j++ {
+			if lines[i][j] == 35 {
+				shape = append(shape, lines[i])
+				break
+			}
+		}
 	}
-	return []byte{35, 35, 35, 35}, nil
+	return shape, nil
+}
+
+func rollShape(shape [][]byte) [][]byte {
+	matrix := shape
+	// Find the dimensions of the matrix
+	rows := len(matrix)
+	cols := len(matrix[0])
+	// Initialize the transposed matrix
+	transposed := make([][]byte, cols)
+	for i := range transposed {
+		transposed[i] = make([]byte, rows)
+	}
+	// Transpose the matrix
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			transposed[j][i] = matrix[i][j]
+		}
+	}
+	return transposed
 }
 
 // checkFormat checks the format of the input file. It takes a string as an argument and returns a boolean.
@@ -54,4 +121,126 @@ func checkFormat(data []byte) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func readShapeMap(data [][]string) string {
+	// Check for I0 shape
+	if len(data) == 4 && len(data[0]) == 1 && len(data[1]) == 1 && len(data[2]) == 1 && len(data[3]) == 1 {
+		if data[0][0] == "#" && data[1][0] == "#" && data[2][0] == "#" && data[3][0] == "#" {
+			return "I0"
+		}
+	}
+	// Check for I1 shape
+	if len(data) == 1 && len(data[0]) == 4 {
+		if data[0][0] == "#" && data[0][1] == "#" && data[0][2] == "#" && data[0][3] == "#" {
+			return "I1"
+		}
+	}
+
+	// Check for J0 {{".","#"}, {".","#"}, {"#", "#"}}
+	if len(data) == 3 && len(data[0]) == 2 && len(data[1]) == 2 && len(data[2]) == 2 {
+		if data[0][1] == "#" && data[1][1] == "#" && data[2][0] == "#" && data[2][1] == "#" {
+			return "J0"
+		}
+	}
+	// Check for J1 {{"#", "#", "#"},{".", ".", "#"}}
+	if len(data) == 2 && len(data[0]) == 3 && len(data[1]) == 3 {
+		if data[0][0] == "#" && data[0][1] == "#" && data[0][2] == "#" && data[1][2] == "#" {
+			return "J1"
+		}
+	}
+	// Check for J2 {{"#", "#"}, {"#", "."}, {"#", "."}}
+	if len(data) == 3 && len(data[0]) == 2 && len(data[1]) == 2 && len(data[2]) == 2 {
+		if data[0][0] == "#" && data[0][1] == "#" && data[1][0] == "#" && data[2][0] == "#" {
+			return "J2"
+		}
+	}
+
+	// Check for J3 {{"#", ".", "."}, {"#", "#", "#"}}
+	if len(data) == 2 && len(data[0]) == 3 && len(data[1]) == 3 {
+		if data[0][0] == "#" && data[1][0] == "#" && data[1][1] == "#" && data[1][2] == "#" {
+			return "J3"
+		}
+	}
+	// Check for L0 {{"#", "."}, {"#", "."}, {"#", "#"}}
+	if len(data) == 3 && len(data[0]) == 2 && len(data[1]) == 2 && len(data[2]) == 2 {
+		if data[0][0] == "#" && data[1][0] == "#" && data[2][0] == "#" && data[2][1] == "#" {
+			return "L0"
+		}
+	}
+	// Check for L1 {{"#", "#", "#"}, {"#", ".", "."}}
+	if len(data) == 2 && len(data[0]) == 3 && len(data[1]) == 3 {
+		if data[0][0] == "#" && data[0][1] == "#" && data[0][2] == "#" && data[1][0] == "#" {
+			return "L1"
+		}
+	}
+
+	// Check for L2 {{"#", "#"}, {".", "#"}, {".", "#"}}
+	if len(data) == 3 && len(data[0]) == 2 && len(data[1]) == 2 && len(data[2]) == 2 {
+		if data[0][0] == "#" && data[0][1] == "#" && data[1][1] == "#" && data[2][1] == "#" {
+			return "L2"
+		}
+	}
+	// Check for L3 {{".", ".", "#"}, {"#", "#", "#"}}
+	if len(data) == 2 && len(data[0]) == 3 && len(data[1]) == 3 {
+		if data[0][2] == "#" && data[1][0] == "#" && data[1][1] == "#" && data[1][2] == "#" {
+			return "L3"
+		}
+	}
+	// Check for O0 shape
+	if len(data) == 2 && len(data[0]) == 2 && len(data[1]) == 2 {
+		if data[0][0] == "#" && data[0][1] == "#" && data[1][0] == "#" && data[1][1] == "#" {
+			return "O0"
+		}
+	}
+
+	// Check for S0 {{".","#","#"}, {"#", "#", "."}}
+	if len(data) == 2 && len(data[0]) == 3 && len(data[1]) == 3 {
+		if data[0][1] == "#" && data[0][2] == "#" && data[1][0] == "#" && data[1][1] == "#" {
+			return "S0"
+		}
+	}
+	// Check for S1 {{"#", "."}, {"#", "#"}, {".", "#"}}
+	if len(data) == 3 && len(data[0]) == 2 && len(data[1]) == 2 && len(data[2]) == 2 {
+		if data[0][0] == "#" && data[1][0] == "#" && data[1][1] == "#" && data[2][1] == "#" {
+			return "S1"
+		}
+	}
+	// Check for T0 {{"#", "#", "#"},{".","#", "."}}
+	if len(data) == 2 && len(data[0]) == 3 && len(data[1]) == 3 {
+		if data[0][0] == "#" && data[0][1] == "#" && data[0][2] == "#" && data[1][1] == "#" {
+			return "T0"
+		}
+	}
+	// Check for T1 {{"#", "."}, {"#", "#"}, {"#", "."}}
+	if len(data) == 3 && len(data[0]) == 2 && len(data[1]) == 2 && len(data[2]) == 2 {
+		if data[0][0] == "#" && data[1][0] == "#" && data[1][1] == "#" && data[2][0] == "#" {
+			return "T1"
+		}
+	}
+	// Check for T2 {{".","#", "."}, {"#", "#", "#"}}
+	if len(data) == 2 && len(data[0]) == 3 && len(data[1]) == 3 {
+		if data[0][1] == "#" && data[1][0] == "#" && data[1][1] == "#" && data[1][2] == "#" {
+			return "T2"
+		}
+	}
+	// Check for T3 {{".", "#"}, {"#", "#"}, {".", "#"}}
+	if len(data) == 3 && len(data[0]) == 2 && len(data[1]) == 2 && len(data[2]) == 2 {
+		if data[0][1] == "#" && data[1][0] == "#" && data[1][1] == "#" && data[2][1] == "#" {
+			return "T3"
+		}
+	}
+	// Check for Z0 {{"#", "#", "."}, {".","#", "#"}}
+	if len(data) == 2 && len(data[0]) == 3 && len(data[1]) == 3 {
+		if data[0][0] == "#" && data[0][1] == "#" && data[1][1] == "#" && data[1][2] == "#" {
+			return "Z0"
+		}
+	}
+	// Check for Z1 {{".", "#"}, {"#", "#"}, {"#", "."}}
+	if len(data) == 3 && len(data[0]) == 2 && len(data[1]) == 2 && len(data[2]) == 2 {
+		if data[0][1] == "#" && data[1][0] == "#" && data[1][1] == "#" && data[2][0] == "#" {
+			return "Z1"
+		}
+	}
+	return "Unknown"
 }
--- optimizer/optimizer_test.go
+++ optimizer/optimizer_test.go
@@ -1,10 +1,24 @@
 package optimizer
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
+	"reflect"
 	"testing"
 )
 
+func TestTetris(t *testing.T) {
+	input := "../example2/example04.txt"
+	expected := "Tetris"
+	actual, err := Tetris(input)
+	if err != nil {
+		t.Errorf("Test failed with error: %v", err)
+	}
+	if actual != expected {
+		t.Errorf("Test failed, expected: '%s', got:  '%s'", expected, actual)
+	}
+}
 func TestCheckFormat(t *testing.T) {
 	input := "../example2/example01.txt"
 	data, err := ioutil.ReadFile(input)
@@ -15,15 +29,50 @@ func TestCheckFormat(t *testing.T) {
 		t.Errorf("Test failed, expected: '%t', got:  '%t' , err: '%v'", expected, actual, err)
 	}
 }
-
 func TestCutShape(t *testing.T) {
 	input := "../example2/example00.txt"
 	data, err := ioutil.ReadFile(input)
-	expected := []byte{35, 35, 35, 35}
-	actual, err := cutShape(data)
-	for i := 0; i < len(expected); i++ {
-		if actual[i] != expected[i] {
-			t.Errorf("Test failed, expected: '%v', got:  '%v' , err: '%v'", expected, actual, err)
+	if err != nil {
+		t.Errorf("Failed to read input file: %v", err)
+	}
+	obj := bytes.Split(data, []byte{'\n', '\n'})
+	lines := [][][]byte{}
+	for _, v := range obj {
+		lines = append(lines, bytes.Split(v, []byte{'\n'}))
+	}
+	expected := [][][]byte{
+		{{35}, {35}, {35}, {35}},
+		{{35, 35, 35, 35}},
+		{{35, 35, 35}, {46, 46, 35}},
+		{{46, 35, 35}, {35, 35, 46}},
+	}
+
+	expect := [][][]byte{}
+	for i := 0; i < len(lines); i++ {
+		value1, err := cutShape(lines[i])
+		value := rollShape(value1)
+		actual, _ := cutShape(value)
+		value = rollShape(actual)
+		if err != nil {
+			t.Errorf("Failed to cut shape: %v", err)
 		}
+		expect = append(expect, value)
+	}
+	if !reflect.DeepEqual(expect, expected) {
+		t.Errorf("Test failed, expected: '%v', got:  '%v' , err: '%v'", expected, expect, err)
+	}
+}
+
+func TestShapeMap(t *testing.T) {
+	input := [][][]string{{{"#"}, {"#"}, {"#"}, {"#"}}, {{"#", "#", "#", "#"}}, {{".", "#"}, {".", "#"}, {"#", "#"}}, {{"#", "#", "#"}, {".", ".", "#"}}, {{"#", "#"}, {"#", "."}, {"#", "."}}, {{"#", ".", "."}, {"#", "#", "#"}}, {{"#", "."}, {"#", "."}, {"#", "#"}}, {{"#", "#", "#"}, {"#", ".", "."}}, {{"#", "#"}, {".", "#"}, {".", "#"}}, {{".", ".", "#"}, {"#", "#", "#"}}, {{"#", "#"}, {"#", "#"}}, {{".", "#", "#"}, {"#", "#", "."}}, {{"#", "."}, {"#", "#"}, {".", "#"}}, {{"#", "#", "#"}, {".", "#", "."}}, {{"#", "."}, {"#", "#"}, {"#", "."}}, {{".", "#", "."}, {"#", "#", "#"}}, {{".", "#"}, {"#", "#"}, {".", "#"}}, {{"#", "#", "."}, {".", "#", "#"}}, {{".", "#"}, {"#", "#"}, {"#", "."}}}
+	for i := 0; i < len(input); i++ {
+		value := readShapeMap(input[i])
+		fmt.Println(value)
+	}
+	input2 := [][]string{{"#"}, {"#"}, {"#"}, {"#"}}
+	str := readShapeMap(input2)
+	expected := "I0"
+	if str != expected {
+		t.Errorf("Test failed, expected: '%s', got:  '%s'", expected, str)
 	}
 }
